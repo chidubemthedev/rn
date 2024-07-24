0590029b28c300ad1467d9fdf2220d51beafa856
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2024-07-24 11:58:09 +0100

    image

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,17 @@
-import { Text, View } from "react-native";
+import LogoImage from "@/assets/images/adaptive-icon.png";
+import { Image, ImageBackground, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum" }}>
-      <Text>
-        <Text style={{ color: "white" }}>Hello</Text> world!
-      </Text>
+      <ImageBackground source={LogoImage} style={{ flex: 1 }}>
+        <Text>Image text</Text>
+      </ImageBackground>
+      <Image source={LogoImage} style={{ width: 300, height: 300 }} />
+      <Image
+        source={{ uri: "https://picsum.photos/320" }}
+        style={{ width: 300, height: 300 }}
+      />
     </View>
   );
 }
