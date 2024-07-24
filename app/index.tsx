0c03e8c04b5fb525016caf80e79fd4ec0743f234
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2024-07-24 18:30:22 +0100

    activity indicator

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,16 @@
-import {
-  Button,
-  Image,
-  Modal,
-  Pressable,
-  StatusBar,
-  Text,
-  View,
-} from "react-native";
-import Logo from "@/assets/images/adaptive-icon.png";
-import { useState } from "react";
+import { ActivityIndicator, View } from "react-native";
 
 export default function Index() {
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 10 }}>
-      <StatusBar backgroundColor="red" barStyle={"default"} hidden />
+      <ActivityIndicator />
+      <ActivityIndicator size={"large"} />
+      <ActivityIndicator size={"large"} color={"midnightblue"} />
+      <ActivityIndicator
+        size={"large"}
+        color={"midnightblue"}
+        animating={false}
+      />
     </View>
   );
 }
