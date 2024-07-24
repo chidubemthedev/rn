21b06b363b1cc811b58194b0918fbe4742b8aa46
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2024-07-24 18:48:37 +0100

    alert

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,29 @@
-import { ActivityIndicator, View } from "react-native";
+import { ActivityIndicator, Alert, Button, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, backgroundColor: "plum", padding: 10 }}>
-      <ActivityIndicator />
-      <ActivityIndicator size={"large"} />
-      <ActivityIndicator size={"large"} color={"midnightblue"} />
-      <ActivityIndicator
-        size={"large"}
-        color={"midnightblue"}
-        animating={false}
+    <View style={{ flex: 1, gap: 2, backgroundColor: "plum", padding: 10 }}>
+      <Button title="Alert" onPress={() => Alert.alert("Invalid Data")} />
+
+      <Button
+        title="Alert 2"
+        onPress={() => Alert.alert("Invalid Data", "DOB Incorrect!")}
+      />
+
+      <Button
+        title="Alert 3"
+        onPress={() =>
+          Alert.alert("Invalid Data", "DOB Incorrect", [
+            {
+              text: "Cancel",
+              onPress: () => console.log("Cancel pressed!"),
+            },
+            {
+              text: "Save",
+              onPress: () => console.log("Save pressed!"),
+            },
+          ])
+        }
       />
     </View>
   );
