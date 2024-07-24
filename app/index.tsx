469a0ab21ca3b2859d06a70645d03a80a0948d14
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2024-07-24 11:38:43 +0100

    text

--- app/index.tsx
+++ app/index.tsx
@@ -3,10 +3,9 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum" }}>
-      <View style={{ backgroundColor: "blue", width: 200, height: 200 }}></View>
-      <View
-        style={{ backgroundColor: "lightblue", width: 200, height: 200 }}
-      ></View>
+      <Text>
+        <Text style={{ color: "white" }}>Hello</Text> world!
+      </Text>
     </View>
   );
 }
