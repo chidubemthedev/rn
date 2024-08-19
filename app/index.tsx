fda51e4ffff2865e2520e33824aa262b42e8a992
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2024-08-19 05:57:24 +0100

    use window dimensions

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,28 @@
 import Box from "@/components/Box";
 import Greet from "@/components/Greet";
-import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { StyleSheet, Text, View, useWindowDimensions } from "react-native";
 
 export default function Index() {
+  const { width, height, fontScale } = useWindowDimensions();
+
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.text}>Hello, World!</Text>
+      <View
+        style={[
+          styles.box,
+          {
+            width: width > 500 ? "70%" : "90%",
+            height: height > 600 ? "60%" : "90%",
+          },
+        ]}
+      >
+        <Text style={{ fontSize: fontScale * 50 }}>
+          {width} x {height} x {fontScale}
+        </Text>
       </View>
     </View>
   );
 }
-
-const { width: windowWidth, height: windowHeight } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   container: {
@@ -22,13 +32,10 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   box: {
-    width: windowWidth > 500 ? "70%" : "90%",
-    height: windowHeight > 600 ? "60%" : "90%",
+    // width: windowWidth > 500 ? "70%" : "90%",
+    // height: windowHeight > 600 ? "60%" : "90%",
     backgroundColor: "lightblue",
     alignItems: "center",
     justifyContent: "center",
-  },
-  text: {
-    fontSize: windowWidth > 500 ? 50 : 24,
   },
 });
