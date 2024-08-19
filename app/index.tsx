4fb3e0e37d5fd0f6b623f33374cc514267a898dc
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2024-08-19 03:49:20 +0100

    setup for flexbox

--- app/index.tsx
+++ app/index.tsx
@@ -1,63 +1,24 @@
+import Box from "@/components/Box";
 import Greet from "@/components/Greet";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <View style={[styles.darkMode]}>
-        <Text style={styles.darkModeText}>Style inheritance</Text>
-      </View>
-
-      <View style={[styles.box, styles.lightblueBg, styles.boxShadow]}>
-        <Text>Lightblue box</Text>
-      </View>
-      <View style={[styles.box, styles.lightgreenBg]}>
-        <Text>Lightgreen box</Text>
-      </View>
-      <View style={[styles.box, styles.lightgreenBg]}>
-        <Text>Lightgreen box</Text>
-      </View>
+      <Box style={{ backgroundColor: "black" }}>Box 1 </Box>
+      <Box style={{ backgroundColor: "green" }}>Box 2 </Box>
+      <Box style={{ backgroundColor: "blue" }}>Box 3 </Box>
+      <Box style={{ backgroundColor: "yellow" }}>Box 4 </Box>
+      <Box style={{ backgroundColor: "purple" }}>Box 5 </Box>
+      <Box style={{ backgroundColor: "pink" }}>Box 6 </Box>
+      <Box style={{ backgroundColor: "orange" }}>Box 7 </Box>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: "column",
-    gap: 2,
-    backgroundColor: "plum",
-    padding: 10,
-    overflow: "scroll",
-  },
-  darkMode: {
-    backgroundColor: "black",
-    marginBottom: 10,
-  },
-  darkModeText: {
-    color: "white",
-    fontSize: 20,
-  },
-  box: {
-    width: "60%",
-    height: 250,
-    padding: 10,
-    borderRadius: 20,
-  },
-  lightblueBg: {
-    backgroundColor: "lightblue",
-  },
-  lightgreenBg: {
-    backgroundColor: "lightgreen",
-  },
-  boxShadow: {
-    shadowColor: "#333",
-    shadowOffset: {
-      width: 6,
-      height: 6,
-    },
-    shadowOpacity: 0.6,
-    shadowRadius: 10,
-    elevation: 10, // for Android
+    borderWidth: 6,
+    borderColor: "red",
   },
 });
---
+++ components/Box.tsx
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+type Props = {
+  children?: React.ReactNode;
+  style: any;
+};
+
+const Box = ({ children, style }: Props) => {
+  return (
+    <View style={[styles.box, style]}>
+      <Text style={styles.text}>{children}</Text>
+    </View>
+  );
+};
+
+export default Box;
+
+const styles = StyleSheet.create({
+  box: {
+    backgroundColor: "white",
+    padding: 20,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "white",
+  },
+});
