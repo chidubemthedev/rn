e2e78ff482dbd138802c41f44fe146ad92fefa2d
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2024-08-19 05:05:36 +0100

    flex grow

--- app/index.tsx
+++ app/index.tsx
@@ -5,17 +5,13 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "black", flexShrink: 1 }}>
-        Box 1 shrink
-      </Box>
-      <Box style={{ backgroundColor: "green", flexShrink: 2 }}>
-        Box 2 shrink
-      </Box>
-      {/* <Box style={{ backgroundColor: "blue" }}>Box 3</Box>
+      <Box style={{ backgroundColor: "black" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "green" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "blue" }}>Box 3</Box>
       <Box style={{ backgroundColor: "yellow" }}>Box 4</Box>
       <Box style={{ backgroundColor: "purple" }}>Box 5</Box>
       <Box style={{ backgroundColor: "pink" }}>Box 6</Box>
-      <Box style={{ backgroundColor: "orange" }}>Box 7</Box> */}
+      <Box style={{ backgroundColor: "orange" }}>Box 7</Box>
     </View>
   );
 }
@@ -23,14 +19,14 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row",
-    alignItems: "flex-start",
+    // flexDirection: "row",
+    // alignItems: "flex-start",
     // flexWrap: "wrap",
     // rowGap: 20,
     // columnGap: 30,
     // gap: 10,
     // height: 300,
-    width: 300,
+    // width: 300,
     backgroundColor: "gray",
     borderWidth: 6,
     borderColor: "red",
--- components/Box.tsx
+++ components/Box.tsx
@@ -20,6 +20,7 @@ const styles = StyleSheet.create({
   box: {
     backgroundColor: "white",
     padding: 20,
+    flexGrow: 1,
   },
   text: {
     fontSize: 24,
