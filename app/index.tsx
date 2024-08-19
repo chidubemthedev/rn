23a5ed5cae706c881989419f9723d779e282a4d1
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2024-08-19 05:35:42 +0100

    dimensions api

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,18 @@
 import Box from "@/components/Box";
 import Greet from "@/components/Greet";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 
 export default function Index() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Text style={styles.text}>Hello, World!</Text>
+      </View>
+    </View>
+  );
 }
+
+const { width: windowWidth, height: windowHeight } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   container: {
@@ -12,5 +20,15 @@ const styles = StyleSheet.create({
     backgroundColor: "plum",
     alignItems: "center",
     justifyContent: "center",
+  },
+  box: {
+    width: windowWidth > 500 ? "70%" : "90%",
+    height: windowHeight > 600 ? "60%" : "90%",
+    backgroundColor: "lightblue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: windowWidth > 500 ? 50 : 24,
   },
 });
