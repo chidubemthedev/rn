06736f8062f63523985654c02d1b624c9b18ae77
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2024-08-19 04:20:20 +0100

    align self

--- app/index.tsx
+++ app/index.tsx
@@ -5,13 +5,19 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "black" }}>Box 1 </Box>
-      <Box style={{ backgroundColor: "green" }}>Box 2 </Box>
-      <Box style={{ backgroundColor: "blue" }}>Box 3 </Box>
-      {/* <Box style={{ backgroundColor: "yellow" }}>Box 4 </Box>
-      <Box style={{ backgroundColor: "purple" }}>Box 5 </Box>
-      <Box style={{ backgroundColor: "pink" }}>Box 6 </Box>
-      <Box style={{ backgroundColor: "orange" }}>Box 7 </Box> */}
+      <Box style={{ backgroundColor: "black", alignSelf: "flex-start" }}>
+        Box 1
+      </Box>
+      <Box style={{ backgroundColor: "green", alignSelf: "flex-end" }}>
+        Box 2
+      </Box>
+      <Box style={{ backgroundColor: "blue", alignSelf: "center" }}>Box 3</Box>
+      <Box style={{ backgroundColor: "yellow", alignSelf: "stretch" }}>
+        Box 4
+      </Box>
+      <Box style={{ backgroundColor: "purple" }}>Box 5</Box>
+      <Box style={{ backgroundColor: "pink" }}>Box 6</Box>
+      <Box style={{ backgroundColor: "orange" }}>Box 7</Box>
     </View>
   );
 }
@@ -19,8 +25,8 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row",
-    alignItems: "baseline",
+    // flexDirection: "row",
+    alignItems: "flex-end",
     borderWidth: 6,
     borderColor: "red",
   },
